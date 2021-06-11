d0114b1c19192f4384645f301063c7b6fb8dcc0b
GitHub Action <action@github.com>
2021-06-11 23:44:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/patimen/converttoapply/services/MyProjectService.kt
+++ src/main/kotlin/com/github/patimen/converttoapply/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.patimen.converttoapply.services
 
+import com.github.patimen.converttoapply.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
